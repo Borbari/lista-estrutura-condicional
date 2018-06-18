54f4514ec2d52f1d64455560ceed1b081cc1049d
Borbari <38558645+Borbari@users.noreply.github.com>
2018-06-18 11:27:03 -0300

    Interface Grafica02

---
+++ InterfaceGrafica02/src/Exemplo08/JFrameBaseInterface.java
@@ -0,0 +1,15 @@
+
+package Exemplo08;
+
+/**
+
+ * @author Alunos
+ */
+public interface JFrameBaseInterface {
+    void gerarTela();
+    void adicionarComponentes();
+    void instanciarComponentes();
+    void gerarLocalizacoes();
+    void gerarDimensoes();
+    
+}
